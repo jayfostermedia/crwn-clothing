28a96b5ff507541852e1a715b9771ef7014d988e
Jason Foster <jayfostermedia@gmail.com>
2020-09-19 23:16:39 -0400

    stripe integration and finishing touches

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_9w8k9vC3v76gBYN8zCkQl9li';
+  const onToken = token => {
+    console.log(token);
+    alert('Payment successful');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='Crwn Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      mount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
